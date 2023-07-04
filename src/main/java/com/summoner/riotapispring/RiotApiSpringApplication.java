b8271bae7600f0c26845fcbaafb2ba8265e93111
Bartłomiej Skorupski <bartmann001@gmail.com>
2023-07-04 02:52:51 +0200

    initial commit

---
+++ src/main/java/com/summoner/riotapispring/RiotApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.summoner.riotapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiotApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiotApiSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/summoner/riotapispring/RiotApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summoner.riotapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiotApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
