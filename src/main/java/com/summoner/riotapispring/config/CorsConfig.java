d16e760d0bf95bd6061caac9d33d7e43f9e27c4e
Bartłomiej Skorupski <bartmann001@gmail.com>
2023-07-05 15:41:42 +0200

    local address env variable cors headers

--- src/main/java/com/summoner/riotapispring/config/CorsConfig.java
+++ src/main/java/com/summoner/riotapispring/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.summoner.riotapispring.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Value("${address.local}")
+    private String localAddress;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:4200", "http://localhost")
+            .allowedOrigins("http://localhost:4200", "http://localhost", localAddress)
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("*")
             .allowCredentials(true);
